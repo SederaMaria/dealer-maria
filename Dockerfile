ecd5cfafb177d32bc90b132852ec7cc27035ff8a
Mark Blas De Los Santos <markblas.dlsantos@gmail.com>
2021-09-11 04:01:05 +0800

    Add configs

---
+++ Dockerfile
@@ -0,0 +1,21 @@
+FROM node:14.0.0
+
+# Common dependencies
+RUN apt-get update -qq \
+  && DEBIAN_FRONTEND=noninteractive apt-get install -y vim \
+  && apt-get clean \
+  && rm -rf /var/cache/apt/archives/* \
+  && rm -rf /var/lib/apt/lists/* /tmp/* /var/tmp/* \
+  && truncate -s 0 /var/log/*log
+
+# Create app directory and copy source
+RUN mkdir -p /app
+WORKDIR /app
+COPY . .
+
+# Install app dependencies
+RUN yarn global add react-scripts
+
+EXPOSE 3000
+
+CMD [ "yarn", "start" ]
---
+++ src/__tests__/App.test.tsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import { configure } from 'enzyme';
+import Adapter from '@wojtekmaj/enzyme-adapter-react-17';
+configure({ adapter: new Adapter() });
+import { act } from 'react-dom/test-utils';
+import { mount } from 'enzyme';
+
+import App from '../App';
+
+describe('Test case for App',() =>{
+  let wrapper;
+
+  it('Should check App components', async () => {
+    await act(async () => {
+        wrapper = mount(<App/>);
+        expect(wrapper.exists()).toBe(true);
+    });
+  });
+
+}) 
