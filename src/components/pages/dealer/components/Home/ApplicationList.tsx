7953be94077229435cb82c2ef6446528c38ae0b5
Mark Blas De Los Santos <markblas.dlsantos@gmail.com>
2021-09-17 22:53:01 +0800

    Add sample data to home

--- src/components/pages/dealer/components/Home/ApplicationList.tsx
+++ src/components/pages/dealer/components/Home/ApplicationList.tsx
@@ -6,19 +6,105 @@ interface Applications {
     key: number;
     name: string;
   }
+  interface Datas {
+    applicationIdentifier: string;
+    applicant: string;
+    coApplicant: string;
+    modelAndYear: string;
+    creditStatus: string;
+    documentStatus: string;
+    daysSubmitted: string;
+    lastUpdated: string;
+  }
   
   const columns: ColumnsType<Applications> = [
     {
-      key: 'name',
-      title: 'Name',
-      dataIndex: 'name',
+      key: 'applicationIdentifier',
+      title: 'Application Identifier',
+      dataIndex: 'applicationIdentifier',
+      render(val) {
+        return (
+            <a href="#"> { val} </a>
+        )
+    }
+    },
+    {
+      key: 'applicant',
+      title: 'Applicant',
+      dataIndex: 'applicant',
+    },
+    {
+      key: 'coApplicant',
+      title: 'Co-Applicant',
+      dataIndex: 'co-applicant',
+    },
+    {
+      key: 'modelAndYear',
+      title: 'Model and Year',
+      dataIndex: 'modelAndYear',
+    },
+    {
+      key: 'creditStatus',
+      title: 'Credit Status',
+      dataIndex: 'creditStatus',
+    },
+    {
+      key: 'documentStatus',
+      title: 'Document Status',
+      dataIndex: 'documentStatus',
+    },
+    {
+      key: 'daysSubmitted',
+      title: 'Days Submitted',
+      dataIndex: 'daysSubmitted',
+    },
+    {
+      key: 'lastUpdated',
+      title: 'Last Updated',
+      dataIndex: 'lastUpdated',
+    },
+  ];
+
+  const data: any = [
+    {
+      applicationIdentifier: "20009280003",
+      applicant: 'Alvin Testco',
+      coApplicant: '',
+      modelAndYear: '2013 FLHX Street Glide',
+      creditStatus: 'Awaiting Credit Decision',
+      documentStatus: 'No Documents',
+      daysSubmitted: '1',
+      lastUpdated: 'Sept 28, 2020 at 11:28 a.m EDT',
+    },
+    {
+      applicationIdentifier: "20009280003",
+      applicant: 'Debbie Delinquent',
+      coApplicant: '',
+      modelAndYear: '2018 XL883L',
+      creditStatus: 'Approved',
+      documentStatus: 'Funded',
+      daysSubmitted: '5',
+      lastUpdated: 'Sept 24, 2020 at 9:24 am EDT',
+    },
+    {
+      applicationIdentifier: "20009280003",
+      applicant: 'Mark S. Credco',
+      coApplicant: 'Deborah Credco',
+      modelAndYear: '2017 XG750A Street Rod',
+      creditStatus: 'Approved with Contingencies',
+      documentStatus: 'No Documents',
+      daysSubmitted: '15',
+      lastUpdated: 'Sep 22, 2020 at 12:45 p.m EDT',
     },
   ];
 
 
+
+
+
 function ApplicationList() {
     return (
-        <Table columns={columns} dataSource={[]} />
+        <Table columns={columns} dataSource={data} />
     )
 }
 
