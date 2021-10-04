87b03a9a94d253999b138ea0474c615e03e7e925
Vipette Ibano <vpibano@gmail.com>
2021-10-04 23:00:30 +0800

    Logout session if any request responded 401

--- src/utils/network.tsx
+++ src/utils/network.tsx
@@ -1,8 +1,32 @@
 import Axios from 'axios';
 import Cookies from 'js-cookie'
+import { Modal } from 'antd';
+import { logout } from './authenticate';
 
 const LOS_API : string | undefined = process.env.REACT_APP_LOS_API 
 const auth_token = Cookies.get('authToken')
+
+// https://axios-http.com/docs/interceptors
+Axios.interceptors.response.use((response) => {
+    // Any status code that lie within the range of 2xx cause this function to trigger
+    return response;
+  }, (error) => {
+    // Any status codes that falls outside the range of 2xx cause this function to trigger
+    if (error.response.status === 401) {
+        Modal.error({
+            centered: true,
+            title: (error.statusText || 'Oops! Error'),
+            content: 'Access denied. Invalid or Expired Token.',
+            onOk: () => {
+                logout()
+                window.location.href = '/login'
+            }
+        })
+    } else {
+        return Promise.reject(error);
+    }
+  }
+)
 
 export const GET = (url: string) => {
     return Axios.get(`${LOS_API}${url}`, {
