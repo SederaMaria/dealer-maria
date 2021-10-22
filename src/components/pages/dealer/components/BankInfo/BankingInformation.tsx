6011e91bcfa810e32357e7fe7fee698487436d52
tahiry-dev <tahirycontact@gmail.com>
2021-10-22 14:28:08 +0300

    LOS-845: make the page responsive

--- src/components/pages/dealer/components/BankInfo/BankingInformation.tsx
+++ src/components/pages/dealer/components/BankInfo/BankingInformation.tsx
@@ -1,10 +1,11 @@
 import React from 'react';
-import { Card, Row, Col, Form, Input, Button, Layout } from 'antd';
+import { Card, Row, Col, Form, Input, Button, Layout, Typography } from 'antd';
 import { MainHeader} from '../../../../layouts'
 import {BankSider} from '../../../../layouts/BankSider';
 import '../../../../layouts/styles/BankInfo.css'
 
 const { Content } = Layout;
+const {Title} = Typography;
 const BankingInformation = () => {
 
     return (
@@ -16,29 +17,25 @@ const BankingInformation = () => {
                     <Content id='main-content'>
                     <div className="bank-info-container">
                         <Card type="inner" title="Lessee Banking Information">
-                            <Row className="largeInput">
-                                <Col span={5}>
-                                <Form.Item label="Bank Name">
-                                      <Input placeholder="Large Input" />
-                                </Form.Item>
+                            <Row gutter={16}>
+                                <Col xs={24} sm={12} md={6}>
+                                    <Title className="largeInput" level={5}>Bank Name</Title>
+                                    <Input placeholder="Large Input" />
                                 </Col> 
             
-                                <Col span={5}> 
-                                    <Form.Item label="ABA Routing Number">  
-                                        <Input placeholder="Large Input" />
-                                    </Form.Item>
+                                <Col xs={24} sm={12} md={6}> 
+                                    <Title className="largeInput" level={5}>ABA Routing Number</Title>
+                                    <Input placeholder="Large Input" />
                                 </Col> 
             
-                                <Col span={5}> 
-                                    <Form.Item label="Account Number">  
-                                        <Input placeholder="Large Input" />
-                                    </Form.Item>
+                                <Col xs={24} sm={12} md={6}> 
+                                    <Title className="largeInput" level={5}>Account Number</Title>
+                                    <Input placeholder="Large Input" />
                                 </Col> 
             
-                                <Col span={5}> 
-                                    <Form.Item label="Checking / Savings Account">   
-                                        <Input placeholder="Large Input" />
-                                    </Form.Item>
+                                <Col xs={24} sm={12} md={6}> 
+                                    <Title className="largeInput" level={5}>Checking/Savings Account</Title>
+                                    <Input  placeholder="Large Input" />
                                 </Col> 
                             </Row>
                             <Row className="submit-bank-info">
