6777b3ddf5c9bffd71063e8a447d50bd6932ae2b
tahiry-dev <tahirycontact@gmail.com>
2021-11-11 14:00:42 +0300

    LOS-856: update all variables to use Camel Case and  use initialValues props in the Form

--- src/components/pages/dealer/Application/BankingInformation.tsx
+++ src/components/pages/dealer/Application/BankingInformation.tsx
@@ -1,11 +1,11 @@
 import React, {useState} from 'react';
 import { network, logger } from '../../../../utils';
-import { Card, Row, Col, Form, Input, Button, Layout, Select } from 'antd';
+import { Card, Row, Col, Form, Input, Button, Layout, Select, message } from 'antd';
 import { MainHeader, BankSider, MainBreadcrumb } from '../../../layouts'
 import '../../../layouts/styles/BankInfo.css'
 
 const { Content } = Layout;
-const {Option} = Select;
+const {Option} = Select
 
 const layout = {
     labelCol: {
@@ -17,80 +17,39 @@ const layout = {
   };
 
   interface Payments {
-        payment_bank_name?: string | number | undefined
+        payment_bank_name?: string | number | undefined 
         payment_aba_routing_number?: string | number | undefined
         payment_account_number?: string | number | undefined
         payment_account_type?: string | number | undefined
+        application_identifier?: number | undefined
     }
+
 
   interface Props {
     leaseApplicationId?: string | undefined
     data?: {
         payment?: Payments
+        application_identifier? : Payments
     }
 }
 
 const BankingInformation: React.FC<Props> = ({leaseApplicationId, data}) => {
-
-    console.log(data?.payment)
-
+    console.log(`data`, data)
     const [bankInfo, setBankinfo] = useState<Array<any>>([])
 
-    const [currentInfo, setCurrentInfo] = useState<any>({
-        paymentBankName: "",
-        paymentAccountNumber: "",
-        paymentAbaRoutingNumber: ""
-    })
+    const appIdentifier = (data?.application_identifier) ? (data?.application_identifier) : leaseApplicationId
 
-    // useEffect(() => {
-    //     try {
-    //         network.GET(`/api/v1/dealers/${leaseApplicationId}/banking-information`)
-    //         .then(res=>{
-    //             setLabel({
-    //                 bank_name: res.data.payment.payment_bank_name,
-    //                 account_number: res.data.payment.payment_account_number,
-    //                 routing_number: res.data.payment.payment_aba_routing_number,
-    //                 account_type: res.data.payment.payment_account_type
-    //             })
-
-    //             setApp_identifier(res.data.application_identifier)
-    //         })  
-    //         .catch(e=>{
-    //             console.log(`no data error test`, e)
-    //         })
-    
-    //       } catch (e) {
-    //         logger.error("fetch banking information Error", e);
-    //       }
-    // }, [])
-
-    const handleChange = (e:any) => {
-        const {name, value} = e.target
-        setCurrentInfo({...currentInfo, [name]:value})
-    }
-
-    const handleClick = () => {
-        
-        if (!currentInfo.paymentBankName || !currentInfo.paymentAccountNumber || !currentInfo.paymentAbaRoutingNumber || !currentInfo.paymentAccountType) return;
-        
-        setCurrentInfo({
-            paymentBankName: currentInfo.paymentBankName,
-            paymentAccountNumber: currentInfo.paymentAccountNumber,
-            paymentAbaRoutingNumber: currentInfo.paymentAbaRoutingNumber,
-            paymentAccountType: currentInfo.paymentAccountType,
-        })
-
+    const onFinish = (values: any) =>{
         try {
-            network.PUT(`/api/v1/dealers/${leaseApplicationId}/banking-information`, currentInfo)
-            .then(res => console.log(`res PUT`, res))
-                
+            network.PUT(`/api/v1/dealers/${leaseApplicationId}/banking-information`, values)
+            message.success("Data saved successfully")
+
           } catch (e) {
             logger.error("Error sending banking information", e)
           }
-          setBankinfo([...bankInfo, currentInfo])
-    }
+          setBankinfo([...bankInfo, values])
+    }   
 
-    console.log(`payment.bank_name`, payment_bank_name)
     return (
         <> 
             <MainHeader />
@@ -102,7 +61,7 @@ const BankingInformation: React.FC<Props> = ({leaseApplicationId, data}) => {
                                 items={[
                                     { text: " Dealers", link_type: "linkto", link: "#" },
                                     { text: " Lease Application", link_type: "linkto", link: "#" },
-                                    { text:  `1234`, link_type: "linkto", link: "#" },
+                                    { text:  `${appIdentifier}`, link_type: "linkto", link: "#" },
                                     { text: " Bank Information", link_type: "linkto", link: "#" },
                                 ]}
                                 />
@@ -113,44 +72,54 @@ const BankingInformation: React.FC<Props> = ({leaseApplicationId, data}) => {
                             <Form 
                                 {...layout}
                                 initialValues={{
-                                    payment_bank_name: data?.payment?.payment_bank_name,
-                                    payment_aba_routing_number: data?.payment?.payment_aba_routing_number,
-                                    payment_account_number : data?.payment?.payment_account_number,
-                                    payment_account_type : data?.payment?.payment_account_type,
-                                }}   
-                                    >
+                                    paymentBankName: data?.payment?.payment_bank_name,
+                                    paymentAbaRoutingNumber: data?.payment?.payment_aba_routing_number,
+                                    paymentAccountNumber : data?.payment?.payment_account_number,
+                                    paymentAccountType : data?.payment?.payment_account_type,
+                                }}  
+                                onFinish = {onFinish} 
+                            >
                                 <Row gutter={16}>
                                     <Col xs={24} sm={12} md={6}>
-                                        <Form.Item className="largeInput" label="Bank Name" name="payment_bank_name" >
-                                            <Input name="payment_bank_name" onChange={handleChange} placeholder={label.bank_name} /> 
+                                        <Form.Item className="largeInput" label="Bank Name" name="paymentBankName" rules={[{
+                                          required : true,
+                                          message : "This field is required"
+                                        }]} >
+                                            <Input placeholder="your bank name" /> 
                                         </Form.Item>
                                     </Col> 
                                 
                                     <Col xs={24} sm={12} md={6}> 
-                                        <Form.Item className="largeInput" label="ABA Routing Number" name="payment_aba_routing_number" >
-                                            <Input name="payment_aba_routing_number" onChange={handleChange} placeholder={label.routing_number} /> 
+                                        <Form.Item className="largeInput" label="ABA Routing Number" name="paymentAbaRoutingNumber" rules={[{
+                                          required : true,
+                                          message : "This field is required"
+                                        }]} >
+                                            <Input  placeholder="your routing number" /> 
                                         </Form.Item>
                                     </Col> 
                                 
                                     <Col xs={24} sm={12} md={6}> 
-                                        <Form.Item className="largeInput" label="Account Number" name="payment_account_number" >
-                                            <Input name="payment_account_number" onChange={handleChange} placeholder={label.account_number} /> 
+                                        <Form.Item className="largeInput" label="Account Number" name="paymentAccountNumber"
+                                        rules={[{
+                                            required : true,
+                                            message : "This field is required"
+                                          }]} >
+                                            <Input placeholder="your account number" /> 
                                         </Form.Item>
                                     </Col> 
                                 
                                     <Col xs={24} sm={12} md={6}> 
-                                        <Form.Item className="largeInput" label="Checking/Savings Account" name="payment_account_type" >
-                                            <select name="payment_account_type" className="select-option" onChange={handleChange}>
-                                              <option value="">{label.account_type ? label.account_type : "Please Select"}</option>
-                                              <option value="checking">Checking</option>
-                                              <option value="savings">Savings</option>
-                                            </select>
+                                        <Form.Item className="largeInput" label="Checking/Savings Account" name="paymentAccountType">
+                                            <Select>
+                                              <Option  value="checking">Checking</Option>
+                                              <Option value="savings">Savings</Option>
+                                            </Select>
                                         </Form.Item>    
                                     </Col> 
                                 </Row>
                                 <Row className="submit-bank-info">
                                     <Col>
-                                        <Button id="submit-bank-info" type="primary" onClick= {handleClick}>Submit Bank Information</Button>
+                                        <Button id="submit-bank-info" htmlType="submit" type="primary" >Submit Bank Information</Button>
                                     </Col>
                                 </Row>
                             </Form>
