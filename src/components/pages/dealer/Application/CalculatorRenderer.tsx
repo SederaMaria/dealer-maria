7679df3f276be46feab6e8a6fe8b74edc2bb2260
Vipette Ibano <vpibano@gmail.com>
2022-02-03 16:58:13 +0800

    LOS-854: Manual prettify code

--- src/components/pages/dealer/Application/CalculatorRenderer.tsx
+++ src/components/pages/dealer/Application/CalculatorRenderer.tsx
@@ -1,43 +1,41 @@
-import React, { useState, useEffect } from 'react'
-import { Spin } from "antd";
-import { Calculator } from '.'
+import React, { useState, useEffect } from 'react';
 import { logger, network } from '../../../../utils';
 
+import { Spin } from 'antd';
+import { Calculator } from '.';
+
 function CalculatorRenderer(props: any) {
-    const leaseApplicationId: string = `${props.match.params.leaseApplicationId}`
-    const [data, setData] = useState<any>()
-    const [loading, setLoading] = useState<boolean>(false)
+  const leaseApplicationId: string = `${props.match.params.leaseApplicationId}`
 
+  const [data, setData] = useState<any>()
+  const [loading, setLoading] = useState<boolean>(false)
 
-    useEffect(() => {
-        getApplicationDetails();
-      },[]);
-      
-      const getApplicationDetails = async () => {
-        if (!loading) {
-          setLoading(true)
-        }
-        try {
-            let data = await network.GET(`/api/v1/dealers/get-details?id=${leaseApplicationId}`)
-            console.log(data.data.data.leaseApplication)
-            setData(data.data.data.leaseApplication)
-        } catch (e) {
-            setLoading(false)
-            logger.error("Error fetching Applicatins", e);
-        }
-        setLoading(false)
-      }
+  const getApplicationDetails = async () => {
+    if (!loading) { setLoading(true) }
 
+    try {
+      let data = await network.GET(`/api/v1/dealers/get-details?id=${leaseApplicationId}`)
+      setData(data.data.data.leaseApplication)
+      setLoading(false)
+    } catch (e) {
+      logger.error("Error fetching Applicatins", e);
+      setLoading(false)
+    }
+  }
 
-    return data ? (
-        <Spin 
-        spinning={loading}
-        size='large'
-        tip='Loading...'
-        >
-            <Calculator data={data}/>
-        </Spin>
-    ) : null ;
+  useEffect(() => {
+    getApplicationDetails();
+  }, [])
+
+  return data ? (
+    <Spin
+      spinning={loading}
+      size='large'
+      tip='Loading...'
+    >
+      <Calculator data={data} />
+    </Spin>
+  ) : null;
 }
 
 export default CalculatorRenderer
