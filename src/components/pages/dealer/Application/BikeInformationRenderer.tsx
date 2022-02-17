57a3bc2677132db11dcd4f9cb4cfe54089615259
tahiry-dev <tahirycontact@gmail.com>
2022-02-17 15:59:22 +0300

    LOS-912: format code

--- src/components/pages/dealer/Application/BikeInformationRenderer.tsx
+++ src/components/pages/dealer/Application/BikeInformationRenderer.tsx
@@ -1,41 +1,36 @@
 import React, { useState, useEffect } from 'react'
-import { Spin } from "antd";
+import { Spin } from 'antd'
 import { BikeInformation } from '.'
-import { logger, network } from '../../../../utils';
+import { logger, network } from '../../../../utils'
 
-function BikeInformationRenderer(props: any)  {
-    const leaseApplicationId: string = `${props.match.params.leaseApplicationId}`
-    const [data, setData] = useState<any>()
-    const [loading, setLoading] = useState<boolean>(false)
+function BikeInformationRenderer(props: any) {
+  const leaseApplicationId: string = `${props.match.params.leaseApplicationId}`
+  const [data, setData] = useState<any>()
+  const [loading, setLoading] = useState<boolean>(false)
 
-  
-    useEffect(() => {
-        getApplicationDetails();
-      },[]);
+  useEffect(() => {
+    getApplicationDetails()
+  }, [])
 
-      const getApplicationDetails = async () => {
-        if (!loading) {
-          setLoading(true)
-        }
-        try {
-            let data = await network.GET(`/api/v1/dealers/get-details?id=${leaseApplicationId}`)
+  const getApplicationDetails = async () => {
+    if (!loading) {
+      setLoading(true)
+    }
+    try {
+      let data = await network.GET(`/api/v1/dealers/get-details?id=${leaseApplicationId}`)
 
-            setData(data.data.data.leaseApplication)
-        } catch (e) {
-            setLoading(false)
-            logger.error("Error fetching Applicatins", e)
-        }
-        setLoading(false)
-      }
+      setData(data.data.data.leaseApplication)
+    } catch (e) {
+      setLoading(false)
+      logger.error('Error fetching Applicatins', e)
+    }
+    setLoading(false)
+  }
 
-      return data ? (
-        <Spin 
-        spinning={loading} 
-        size='large' 
-        tip='Loading...'
-        >
-             <BikeInformation dataCheck={data?.leaseCalculator.assetMake} data={data} />
-        </Spin>
+  return data ? (
+    <Spin spinning={loading} size="large" tip="Loading...">
+      <BikeInformation dataCheck={data?.leaseCalculator.assetMake} data={data} />
+    </Spin>
   ) : null
 }
 
